24e83f7c9b83ea700b618545ab7817fc3235f84f
EduardoPandolfo <ekpandolfo@gmail.com>
2023-09-25 15:20:28 -0300

    inicio da seção de OO

---
+++ objeto/oo.js
@@ -0,0 +1,26 @@
+//procedural
+processamento(valor1, valor2, valor3);
+
+function processamento() {
+
+}
+
+//OO
+const objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        //...
+    }
+}
+
+objeto.processamento(); //foco passou a ser o objeto
+
+/*
+    princípios importantes
+    1. abstração
+    2. encapsulamento
+    3. herança (prototype)
+    4. polimorfismo
+ */
