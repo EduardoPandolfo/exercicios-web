6e9844fddfa99bee6a0a6846147d11c7fd2e7ab3
EduardoPandolfo <ekpandolfo@gmail.com>
2023-09-28 09:10:48 -0300

    implementação do desafio

--- node/funcionario/funcionario.js
+++ node/funcionario/funcionario.js
@@ -1,9 +1,16 @@
 const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json';
 const axios = require('axios');
 
-axios.get(url).then(response => {
-    const funcionarios = response.data;
-    console.log(funcionarios);
-});
 
 //Desafio - selecionar a mulher chinesa com o menor salário
+
+//const toJson = (text) => JSON.parse(text);
+const filterPaisChina = registro => registro.pais === 'China';
+const filterGeneroFemenino = (registro) => registro.genero === 'F';
+const menorSalario = (funcionario, funcionarioAtual) => funcionario.salario < funcionarioAtual.salario ? funcionario : funcionarioAtual;
+
+axios.get(url).then(response => {
+    const funcionarios = response.data;
+    //console.log(funcionarios);
+    console.log(funcionarios.filter(filterPaisChina).filter(filterGeneroFemenino).reduce(menorSalario));
+});
